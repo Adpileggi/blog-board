ad6995730094c28c0719a7577773915b5cc3d2bc
Anthony Pileggi <adpileggi@gmail.com>
2024-03-25 18:29:53 -0400

    added hashing to seeded passwords

--- seeds/userSeed.js
+++ seeds/userSeed.js
@@ -1,17 +1,18 @@
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 const userData = [
     {
         name: 'Hagrid',
-        password: 'biganimals'
+        password: bcrypt.hashSync('biganimals', 10)
     },
     {
         name: 'Harry',
-        password: 'broomstick'
+        password: bcrypt.hashSync('broomstick', 10)
     },
     {
         name: 'Doby',
-        password: 'fuzzysocks'
+        password: bcrypt.hashSync('fuzzysocks', 10)
     }
 ];
 
